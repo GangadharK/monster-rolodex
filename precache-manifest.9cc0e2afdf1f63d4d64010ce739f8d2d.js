c396d1037d5380bb841902b6da8dc996599bc05b
GangadharK <Gangadhar.Karala@gmail.com>
2019-09-29 19:20:59 +0530

    Updates

---
+++ precache-manifest.9cc0e2afdf1f63d4d64010ce739f8d2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7684db17e37f93796a201cd9f04ff25b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1e7e968b94abfd109d49",
+    "url": "/monster-rolodex/static/css/main.cecbcb74.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monster-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "1e7e968b94abfd109d49",
+    "url": "/monster-rolodex/static/js/main.52a3b41d.chunk.js"
+  },
+  {
+    "revision": "b9d608cb1517ab0e40b5",
+    "url": "/monster-rolodex/static/js/runtime~main.8833da29.js"
+  }
+]);
